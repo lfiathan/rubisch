d3d90ea585c93ddc90d354c4b7e3bf424caafc54
Ivanrhmt77 <ivanrahmat.p77@gmail.com>
2025-05-25 21:16:26 +0700

    Menambahkan komponen coin information

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:rubisch/components/carousel.dart';
+import 'package:rubisch/components/coin_information.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -12,9 +13,11 @@ class HomePage extends StatelessWidget {
         padding: EdgeInsets.symmetric(horizontal: 16.w),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 32.h,
           children: [
-            SizedBox(height: 32.h),
+            SizedBox(),
             Column(
+              spacing: 8.h,
               children: [
                 Text(
                   "Hello User !",
@@ -22,19 +25,23 @@ class HomePage extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                SizedBox(height: 8.h),
                 Text(
                   "Let's turn waste into value!",
                   style: Theme.of(context).textTheme.labelLarge,
                 ),
               ],
             ),
-            SizedBox(height: 32.h),
-            CarouselWidget(
-              imageUrls: [
-                'https://penjagalaut.org/wp-content/uploads/2023/01/2-1015x675.png',
-                'https://yiari.or.id/wp-content/uploads/2025/01/daur.webp',
-                'https://pict.sindonews.net/dyn/732/pena/news/2020/05/14/45/28828/inilah-10-negara-terbaik-pendaur-ulang-sampah-kvf.jpg',
+            Column(
+              spacing: 16.h,
+              children: [
+                CarouselWidget(
+                  imageUrls: [
+                    'https://penjagalaut.org/wp-content/uploads/2023/01/2-1015x675.png',
+                    'https://yiari.or.id/wp-content/uploads/2025/01/daur.webp',
+                    'https://pict.sindonews.net/dyn/732/pena/news/2020/05/14/45/28828/inilah-10-negara-terbaik-pendaur-ulang-sampah-kvf.jpg',
+                  ],
+                ),
+                CoinInformation(coin: "1200"),
               ],
             ),
           ],
